9b45d586f5ab56e238f0db9e1f6080a543d34988
Alexander Koryttsev <alex@Mac-mini-Alexander.local>
2014-05-31 17:23:24 +0300

    Initial Commit

---
+++ Tool Kit/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  Tool Kit
+//
+//  Created by Alexander Koryttsev on 5/31/14.
+//  Copyright (c) 2014 Alexander Koryttsev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tool KitTests/Tool_KitTests.m
@@ -0,0 +1,34 @@
+//
+//  Tool_KitTests.m
+//  Tool KitTests
+//
+//  Created by Alexander Koryttsev on 5/31/14.
+//  Copyright (c) 2014 Alexander Koryttsev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tool_KitTests : XCTestCase
+
+@end
+
+@implementation Tool_KitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
